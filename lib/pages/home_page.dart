5a60ec02c1bedde8473664434343c1b5f4a3a054
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2023-12-11 19:31:12 +0200

    feat: update home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:to_do_app/models/todo_list.model.dart';
+import 'package:to_do_app/models/todo_list.dart';
 import 'package:to_do_app/widgets/floating_action_btn.dart';
 import 'package:to_do_app/widgets/todo_tile.dart';
 
@@ -11,29 +11,34 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final todoList = context.watch<TodoList>();
 
-    final todoListItems = todoList.items;
+    final todoListItems = todoList.items!;
     return Scaffold(
         backgroundColor: Theme.of(context).colorScheme.background,
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.primary,
           title: const Text("TO DO"),
         ),
-        body: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 36),
-            child: ListView.builder(
-                itemCount: todoListItems.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    margin: const EdgeInsets.only(bottom: 24),
-                    child: TodoTile(
-                      todoItem: todoListItems[index],
-                      // ignore: avoid_types_as_parameter_names
-                      onChanged: (bool) =>
-                          todoList.toggleTodoIsCompleted(index),
-                      deleteItem: (context) => todoList.deleteItem(index),
-                    ),
-                  );
-                })),
+        body: todoListItems.isEmpty
+            ? const Center(
+                child: Text("Add your first task"),
+              )
+            : Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 36),
+                child: ListView.builder(
+                    itemCount: todoListItems.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: const EdgeInsets.only(bottom: 24),
+                        child: TodoTile(
+                          todoItem: todoListItems[index],
+                          // ignore: avoid_types_as_parameter_names
+                          onChanged: (bool) =>
+                              todoList.toggleTodoIsCompleted(index),
+                          deleteItem: (context) => todoList.deleteItem(index),
+                        ),
+                      );
+                    })),
         floatingActionButton: const FloatingActionBtn());
   }
 }
